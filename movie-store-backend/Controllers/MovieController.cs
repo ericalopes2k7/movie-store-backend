d66dd8d0ed311b8a187467cb764cf7a9f523ab54
Érica Lopes <ericamlopes21@hotmail.com>
2023-09-28 18:38:57 +0100

    Created Movie Controller

---
+++ movie-store-backend/Controllers/MovieController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace movie_store_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        [HttpPost]
+        public ActionResult CreateMovie()
+        {
+            return StatusCode(StatusCodes.Status201Created, "Movie created");
+        }
+
+        [HttpGet]
+        public ActionResult FindMovie()
+        {
+            return StatusCode(StatusCodes.Status200OK, "Movie found");
+        }
+
+        [HttpGet("search")]
+        public ActionResult FindMovies()
+        {
+            return StatusCode(StatusCodes.Status200OK, "Movies found");
+        }
+
+        [HttpGet("all")]
+        public ActionResult FindAllMovies()
+        {
+            return StatusCode(StatusCodes.Status200OK, "All Movies were found");
+        }
+
+        [HttpPut]
+        public ActionResult UpdateMovie()
+        {
+            return StatusCode(StatusCodes.Status200OK, "Movie updated");
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteMovie()
+        {
+            return StatusCode(StatusCodes.Status200OK, "Movie deleted");
+        }
+    }
+}
