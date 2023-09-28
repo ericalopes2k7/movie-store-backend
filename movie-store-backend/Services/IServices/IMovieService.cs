0b716f6e731252e140be651887001243ce1a6174
Érica Lopes <ericamlopes21@hotmail.com>
2023-09-28 19:01:55 +0100

    Created Movie Service

---
+++ movie-store-backend/Services/IServices/IMovieService.cs
@@ -0,0 +1,12 @@
+﻿namespace movie_store_backend.Services.IServices
+{
+    public interface IMovieService
+    {
+        string CreateMovie();
+        string FindMovie();
+        string FindMovies();
+        string FindAllMovies();
+        string UpdateMovie();
+        string DeleteMovie();
+    }
+}
---
+++ movie-store-backend/Services/MovieService.cs
@@ -0,0 +1,37 @@
+﻿using movie_store_backend.Services.IServices;
+
+namespace movie_store_backend.Services
+{
+    public class MovieService : IMovieService
+    {
+        public string CreateMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FindMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FindMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FindAllMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpdateMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DeleteMovie()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
