61d01daee8914a5c95fb3589981ebe1b128e6848
Érica Lopes <ericamlopes21@hotmail.com>
2023-09-29 21:57:08 +0100

    Created MovieDTO

---
+++ movie-store-backend/DTOs/MovieDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace movie_store_backend.DTOs
+{
+    public class MovieDTO
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+    }
+}
