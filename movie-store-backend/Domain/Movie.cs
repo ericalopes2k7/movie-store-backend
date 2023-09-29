be72d601f3d53fdaa080863696b73fc1d6d60497
Érica Lopes <ericamlopes21@hotmail.com>
2023-09-29 21:55:29 +0100

    Created a domain class for movies

---
+++ movie-store-backend/Domain/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace movie_store_backend.Domain
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+
+        public Movie(string _title, string _synopsis, string _director, int _year)
+        {
+            Title = _title;
+            Synopsis = _synopsis;
+            Director = _director;
+            Year = _year;
+        }
+    }
+}
