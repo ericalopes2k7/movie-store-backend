55d31c2a2d66eca61d5c8c621232053f7319bb47
Érica Lopes <ericamlopes21@hotmail.com>
2023-09-29 22:17:17 +0100

    Added movie id

--- movie-store-backend/Domain/Movie.cs
+++ movie-store-backend/Domain/Movie.cs
@@ -2,6 +2,7 @@
 {
     public class Movie
     {
+        public Guid Id { get; }
         public string Title { get; set; }
         public string Synopsis { get; set; }
         public string Director { get; set; }
@@ -9,6 +10,7 @@
 
         public Movie(string _title, string _synopsis, string _director, int _year)
         {
+            Id = Guid.NewGuid();
             Title = _title;
             Synopsis = _synopsis;
             Director = _director;
