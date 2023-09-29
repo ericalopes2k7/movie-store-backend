777d4cd690e29c21b29b0795e7857c0e1d8746b9
Érica Lopes <ericamlopes21@hotmail.com>
2023-09-29 19:03:05 +0100

    Created a class for movie request body validation

---
+++ movie-store-backend/DTOs/MovieRequestBody.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movie_store_backend.DTOs
+{
+    public class MovieRequestBody
+    {
+        [Required]
+        [StringLength(96, MinimumLength = 2)]
+        public required string Title { get; set; }
+
+        [Required]
+        [StringLength(512, MinimumLength = 2)]
+        public required string Synopsis { get; set; }
+
+        [Required]
+        [StringLength(96, MinimumLength = 2)]
+        public required string Director { get; set; }
+
+        [Required]
+        [Range(1888, int.MaxValue)]
+        public int Year { get; set; }
+    }
+}
