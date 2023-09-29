52d9adef02a7c29834256a697d34c58f08fdc26a
Érica Lopes <ericamlopes21@hotmail.com>
2023-09-29 22:19:13 +0100

    Created the MovieMapper class with the ToDTO method

---
+++ movie-store-backend/Mapper/MovieMapper.cs
@@ -0,0 +1,20 @@
+﻿using movie_store_backend.Domain;
+using movie_store_backend.DTOs;
+
+namespace movie_store_backend.Mapper
+{
+    public class MovieMapper
+    {
+        public static MovieDTO ToDTO(Movie movie)
+        {
+            return new MovieDTO
+            {
+                Id = movie.Id.ToString(),
+                Title = movie.Title,
+                Synopsis = movie.Synopsis,
+                Director = movie.Director,
+                Year = movie.Year
+            };
+        }
+    }
+}
