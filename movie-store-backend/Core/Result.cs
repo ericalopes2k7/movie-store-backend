b1c144c36f5b8d39cda77f1614258ac2405ca552
Érica Lopes <ericamlopes21@hotmail.com>
2023-09-29 23:07:13 +0100

    Added Result class and the methods ok() and fail()

---
+++ movie-store-backend/Core/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace movie_store_backend.Core
+{
+    public class Result<T> where T : class
+    {
+        public bool isSuccess { get; }
+        public string? errorMessage { get; }
+        public T? value { get; }
+
+        public Result(bool isSuccess, string? errorMessage, T? value)
+        {
+            this.isSuccess = isSuccess;
+            this.errorMessage = errorMessage;
+            this.value = value;
+        }
+
+        public static Result<T> Ok(T value)
+        {
+            return new Result<T>(true, null, value);
+        }
+
+        public static Result<T> Fail(string errorMessage)
+        {
+            return new Result<T>(false, errorMessage, null);
+        }
+    }
+}
