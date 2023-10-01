854ced15457078a35f8aaa0c551c8bcaeb2cdf3e
Érica Lopes <ericamlopes21@hotmail.com>
2023-10-01 15:49:27 +0100

    Created AppDBContext class defining the database as inMemory

---
+++ movie-store-backend/Core/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using movie_store_backend.Domain;
+
+namespace movie_store_backend.Core
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "movieStore");
+        }
+    }
+}
