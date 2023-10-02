561df1d372a35df0cf0fff3493e19bc082995255
Érica Lopes <ericamlopes21@hotmail.com>
2023-10-02 22:27:24 +0100

    Created MovieRepo for specific movie entity methods

---
+++ movie-store-backend/Repos/IRepos/IMovieRepo.cs
@@ -0,0 +1,9 @@
+﻿using movie_store_backend.Domain;
+
+namespace movie_store_backend.Repos.IRepos
+{
+    public interface IMovieRepo : IRepo<Movie>
+    {
+        Task<List<Movie>> FindMany(string title);
+    }
+}
---
+++ movie-store-backend/Repos/MovieRepo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using movie_store_backend.Core;
+using movie_store_backend.Domain;
+using movie_store_backend.Repos.IRepos;
+
+namespace movie_store_backend.Repos
+{
+    public class MovieRepo : Repo<Movie>, IMovieRepo
+    {
+        public MovieRepo(AppDBContext dbContext) : base(dbContext, dbContext.Movies) { }
+
+        public async Task<List<Movie>> FindMany(string title)
+        {
+            var ty= await _dbset.Where(x => x.Title.Contains(title)).ToListAsync();
+            return ty;
+        }
+    }
+}
