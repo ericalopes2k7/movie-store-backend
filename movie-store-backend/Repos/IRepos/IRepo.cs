c6268df2e856811e2b40bd93a7b60ba7f21c76f2
Érica Lopes <ericamlopes21@hotmail.com>
2023-10-02 22:25:29 +0100

    Created Repo with standard methods for all entities

---
+++ movie-store-backend/Repos/IRepos/IRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace movie_store_backend.Repos.IRepos
+{
+    public interface IRepo<TEntity> 
+    {
+        Task Create(TEntity obj);
+        Task<List<TEntity>> FindAll();
+        Task<TEntity?> FindOne(string id);
+        Task Delete(TEntity obj);
+        Task CommitChanges();
+    }
+}
---
+++ movie-store-backend/Repos/Repo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using movie_store_backend.Core;
+using movie_store_backend.Repos.IRepos;
+
+namespace movie_store_backend.Repos
+{
+    public class Repo<TEntity> : IRepo<TEntity> where TEntity : Entity
+    {
+        private readonly AppDBContext _dbContext;
+        protected readonly DbSet<TEntity> _dbset;
+
+        public Repo(AppDBContext dbContext, DbSet<TEntity> dbset)
+        {
+            _dbContext = dbContext;
+            _dbset = dbset;
+        }
+
+        public async Task Create(TEntity obj)
+        {
+            await _dbset.AddAsync(obj);
+            await CommitChanges();
+        }
+
+        public async Task<List<TEntity>> FindAll()
+        {
+            return await _dbset.ToListAsync();
+        }
+
+        public async Task<TEntity?> FindOne(string id)
+        {
+            return await _dbset.Where(x => x.Id.ToString().Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task Delete(TEntity obj)
+        {
+            _dbset.Remove(obj);
+            await CommitChanges();
+        }
+
+        public async Task CommitChanges()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
