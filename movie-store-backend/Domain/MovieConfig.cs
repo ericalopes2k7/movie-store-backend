7ffbbc5a9e46b81839363ce6bc69eacbcbdf14f1
Érica Lopes <ericamlopes21@hotmail.com>
2023-10-02 22:24:14 +0100

    Created MovieConfig class to define id as primary key

---
+++ movie-store-backend/Domain/MovieConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace movie_store_backend.Domain
+{
+    public class MovieConfig : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
