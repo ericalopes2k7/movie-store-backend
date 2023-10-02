3a97b9ebb1d4c51f1da623e4eec93c83d3ad668e
Érica Lopes <ericamlopes21@hotmail.com>
2023-10-02 22:22:54 +0100

    Created Entity class with id field

---
+++ movie-store-backend/Core/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace movie_store_backend.Core
+{
+    public class Entity
+    {
+        public Guid Id { get; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
