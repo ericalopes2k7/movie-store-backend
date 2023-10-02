188a458e8083f8753983b66c5937903161ce4c2b
Érica Lopes <ericamlopes21@hotmail.com>
2023-10-02 22:28:31 +0100

    Added dependency injection for MovieRepo

--- movie-store-backend/Program.cs
+++ movie-store-backend/Program.cs
@@ -1,10 +1,15 @@
 using movie_store_backend.Services.IServices;
 using movie_store_backend.Services;
+using movie_store_backend.Repos;
+using movie_store_backend.Repos.IRepos;
+using movie_store_backend.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //CUSTOM
-builder.Services.AddSingleton<IMovieService, MovieService>();
+builder.Services.AddDbContext<AppDBContext>();
+builder.Services.AddTransient<IMovieRepo, MovieRepo>();
+builder.Services.AddTransient<IMovieService, MovieService>();
 //CUSTOM
 
 // Add services to the container.
